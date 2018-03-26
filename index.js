a0fa48100acdb46fd49178601b8d1cba47f640aa
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-27 01:41:53 +0700

    Fix semi-colon

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const sumOddDataFromEvenData = data
   .filter(value => value % 2 == 0)
   .map(value => value + 1)
-  .reduce((previousNumber, value) => previousNumber + value);
+  .reduce((previousNumber, value) => previousNumber + value)
 
 const objectData = [
   {
@@ -11,6 +11,6 @@ const objectData = [
   {
     values: [2, 2]
   }
-];
-const pluckObjectData = objectData.map(({ values }) => values);
-console.log(pluckObjectData);
+]
+const pluckObjectData = objectData.map(({ values }) => values)
+console.log(pluckObjectData)
