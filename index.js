18684bffda8a611212c4004229dc12866ee8a0db
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-27 01:30:21 +0700

    Add pluckObjectWithMap

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const sumOddDataFromEvenData = data
   .filter(value => value % 2 == 0)
   .map(value => value + 1)
   .reduce((previousNumber, value) => previousNumber + value);
+
+const objectData = [
+  {
+    values: [0, 0]
+  },
+  {
+    values: [2, 2]
+  }
+];
+const pluckObjectData = objectData.map(({ values }) => values);
+console.log(pluckObjectData);
